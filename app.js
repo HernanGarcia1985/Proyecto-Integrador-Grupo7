23d9a0dcd8981b12d7a4eef0ea681272207eb59e
HernanGarcia1985 <hernang1985@gmail.com>
2021-06-30 18:50:59 -0300

    Primer commit - Estructura

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, './public')));
+
+
+app.listen(3000, function() {
+  console.log("Corriendo servidor en el puerto 3000");
+});
+
