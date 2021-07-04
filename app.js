6f14f4061c6a58a7b81caed61b975121c4dd402d
Fabi88code <85083301+Fabi88code@users.noreply.github.com>
2021-07-04 19:52:17 -0300

    Actualizado para que ande todo

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const path = require('path');
 
 app.get('/', (req,res) =>{
   
-  res.sendFile(path.join(__dirname, './public/views/index.html'));
+  res.sendFile(path.join(__dirname, './views/index.html'));
   console.log(__dirname)
 });
 
-app.use(express.static(path.join(__dirname, './public')));
+app.get('/producto', (req,res) =>{
+  
+  res.sendFile(path.join(__dirname, './views/producto.html'));
+  console.log(__dirname)
+});
 
+
+
+app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './views')));
 
 app.listen(3000, function() {
   console.log("Corriendo servidor en el puerto 3000");
