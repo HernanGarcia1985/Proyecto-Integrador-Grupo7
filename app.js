444d79e41bf3418f7d5978efdf573f378410cbb4
Andre Guillen <andreguillen@gmail.com>
2021-07-04 00:46:27 -0300

    Agregue carpeta public-Css-imagenes

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const path = require('path');
 
 app.get('/', (req,res) =>{
-  res.sendFile(path.join(__dirname, './views/index.html'));
+  
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+  console.log(__dirname)
 });
 
 app.use(express.static(path.join(__dirname, './public')));
