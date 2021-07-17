3808e038a53d94298b84c4d08692710a29a121f6
Fabi88code <85083301+Fabi88code@users.noreply.github.com>
2021-07-16 21:29:53 -0300

    sin encontrar la posible solución a que se visualice

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,15 +1,15 @@
-const productsControllers = require('./../controllers/productsControllers')
+const productsController = require('./../controllers/productsController')
 
 const express = require('express');
 const router = express.Router();
 
-router.get('/detalle_producto',productsControllers.detalle_producto)
+router.get('/detalle_producto',productsController.detalle_producto)
 
-router.get('/crear_producto',productsControllers.crear_producto)
+router.get('/crear_producto',productsController.crear_producto)
 
-router.get('/editar_producto',productsControllers.editar_producto)
+router.get('/editar_producto',productsController.editar_producto)
 
-router.get('/listado_producto',productsControllers.listado_producto)
+router.get('/listado_producto',productsController.listado_producto)
 
 
 module.exports = router;
