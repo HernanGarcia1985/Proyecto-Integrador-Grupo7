13965c152662456162cc55b27930eef65999f122
Andre Guillen <andreguillen@gmail.com>
2021-08-04 19:34:39 -0300

    instalacion multer- productsRouter

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -2,6 +2,22 @@ const productsController = require('./../controllers/productsController')
 
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+//***  Multer configuration  ****/
+
+const configuracionImagen = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/img/productos'));    // Ruta donde almacenamos el archivo/en este caso en una carpeta productos dentro de imagenes
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imageName =  Date.now() + file.originalname ;   // milisegundos y extensión de archivo original
+     cb(null, imageName);         
+    }
+});
+
+
+const uploadFile = multer({ storage: configuracionImagen });
+
 
 
 
@@ -11,7 +27,7 @@ router.get('/create',productsController.crear_producto); //products/create
 
 router.get('/:id',productsController.detalle_producto);  //products/:id
 
-//router.put('/:id',productsController.editar_producto2); //products/:id editar
+router.get('/edit/:id',productsController.edit); //products/:id editar
 
 //router.delete('/:id',productsController.editar_producto3); //products/:id
 
@@ -21,3 +37,7 @@ router.get('/',productsController.listado_producto); //products
 
 
 module.exports = router;
+
+/*/*** EDIT ONE PRODUCT ***/ 
+//router.get('/edit/:id', productsController.edit); 
+//router.put('/edit/:id', productsController.update); 
