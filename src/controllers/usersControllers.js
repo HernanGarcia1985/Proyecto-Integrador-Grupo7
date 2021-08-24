914c64dce5914b5bfbafda11bec72af0ed958c93
HernanGarcia1985 <hernang1985@gmail.com>
2021-08-24 15:18:50 -0300

    Se agrego metodos procesoLogin y procesoCrearCeunta en usersControlllers.js

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,3 +1,6 @@
+//const bcryptjs = require('bcryptjs');
+//const {validationResult} = require('express-validator');
+
 const User = require('../models/usersModel');
 
 let usuario = {
@@ -21,9 +24,87 @@ const usersControllers = {
 
         crear_cuenta: (req, res) => {
                 res.render('users/crear_cuenta');
-        }
+        },
 
-		
+        procesoLogin: (req, res) => {
+		//let usuarioLogin = User.buscarPorEmail(req.body.email);
+			res.redirect('/users/perfil');
+		}
+/*		
+		if(usuarioLogin) {
+			let validarPassword = bcryptjs.compareSync(req.body.password, usuarioLogin.password);
+			if (validarPassword) {
+				delete usuarioLogin.password;
+				req.session.usuarioLogueado = usuarioLogin;
+
+				if(req.body.remember_user) {
+					res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
+				} //Opcion de recordar usuario
+
+				return res.redirect('/users/perfil');
+			} 
+			return res.render('login', {
+				errors: {
+					email: {
+						msg: 'Las credenciales son inválidas'
+					}
+				}
+			});
+		}
+
+		return res.render('login', {
+			errors: {
+				email: {
+					msg: 'No se encuentra este email en nuestra base de datos'
+				}
+			}
+		});
+	},
+/*
+        procesoCrearCuenta: (req, res) => {
+		const resultValidation = validationResult(req);
+
+		if (resultValidation.errors.length > 0) {
+			return res.render('crear_cuenta', {
+				errors: resultValidation.mapped(),
+				oldData: req.body
+			});
+		}
+
+		let usuarioEnBD = User.buscarPorEmail(req.body.email);
+
+		if (usuarioEnBD) {
+			return res.render('crear_cuenta', {
+				errors: {
+					email: {
+						msg: 'Este email ya está registrado'
+					}
+				},
+				oldData: req.body
+			});
+		}
+
+		let usuarioPorCrear = {
+			...req.body,
+			password: bcryptjs.hashSync(req.body.password, 10),
+			image: req.file.filename
+		}
+
+		let usuarioCreado = User.crear(usuarioPorCrear);
+
+		return res.redirect('/users/login');
+
+	profile: (req, res) => {
+		return res.render('userProfile', {
+			user: req.session.usuarioLogueado
+		});
+	},
+
+	logout: (req, res) => {
+		res.clearCookie('userEmail');
+		req.session.destroy();
+		return res.redirect('/');
+	}*/
 }
 
 
