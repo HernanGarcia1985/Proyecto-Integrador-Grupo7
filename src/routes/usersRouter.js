15946d786c9f10a499b6bc3b30da5f9923e7d649
HernanGarcia1985 <hernang1985@gmail.com>
2021-08-24 15:16:34 -0300

    Se agrego la ruta POST login

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,11 +3,13 @@ const usersControllers = require('../controllers/usersControllers')
 const express = require('express');
 const router = express.Router();
 
-router.get('/crear_cuenta',usersControllers.crear_cuenta)
+router.get('/crear_cuenta',usersControllers.crear_cuenta);
 
-router.get('/login',usersControllers.login)
+router.get('/login',usersControllers.login);
 
-router.get('/perfil',usersControllers.perfil)
+router.post('/login', usersControllers.procesoLogin);
+
+router.get('/perfil',usersControllers.perfil);
 
 
 module.exports = router;
