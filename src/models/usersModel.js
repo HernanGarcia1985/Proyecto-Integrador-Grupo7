ae5dce28152077152a3f4f7c8dcbfddad0e81a22
HernanGarcia1985 <hernang1985@gmail.com>
2021-08-24 10:39:14 -0300

    Creacion de usersModel.js y logica de buscar-crear-borrar usuarios

---
+++ src/models/usersModel.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+const User = {
+	fileName: './data/usuarios-pet.json',
+
+	leerBD: function () {
+		return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
+	},
+
+	generarId: function () {
+		let todosLosUsuarios = this.encontrarTodos();
+		let ultimoUsuario = todosLosUsuarios.pop();
+		if (ultimoUsuario) {
+			return ultimoUsuario.id + 1;
+		}
+		return 1;
+	},
+
+	encontrarTodos: function () {
+		return this.leerBD();
+	},
+
+	buscarPorPK: function (id) {
+		let todosLosUsuarios = this.encontrarTodos();
+		let usuarioEncontrado = todosLosUsuarios.find(unUsuario => unUsuario.id === id);
+		return usuarioEncontrado;
+	},
+
+	buscarPorEmail: function (email) {
+		let todosLosUsuarios = this.encontrarTodos();
+		let usuarioEncontrado = todosLosUsuarios.find(unUsuario => unUsuario['email'] === email);
+		return usuarioEncontrado;
+	},
+
+	crear: function (userData) {
+		let todosLosUsuarios = this.encontrarTodos();
+		let nuevoUsuario = {
+			id: this.generarId(),
+			...userData
+		}
+		todosLosUsuarios.push(nuevoUsuario);
+		fs.writeFileSync(this.fileName, JSON.stringify(todosLosUsuarios, null,  ' '));
+		return nuevoUsuario;
+	},
+
+	borrar: function (id) {
+		let todosLosUsuarios = this.encontrarTodos();
+		let usuariosDefinitivos = todosLosUsuarios.filter(unUsuario => unUsuario.id !== id);
+		fs.writeFileSync(this.fileName, JSON.stringify(usuariosDefinitivos, null, ' '));
+		return true;
+	}
+}
+
+module.exports = User;
