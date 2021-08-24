65737d5f6be0649d5ec840f1609505995bc7df6b
HernanGarcia1985 <hernang1985@gmail.com>
2021-08-24 10:30:57 -0300

    Creacion vista perfil

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,7 +1,18 @@
-const usersControllers = 
-{
+const User = require('../models/usersModel');
+
+let usuario = {
+	id:1,
+	name:"Katerina",
+	lastname: "Baudins",
+	email:"kbaudins0@diigo.com",
+	password:"LQcpPlvddkA0",
+	image:"http://dummyimage.com/108x100.png/ff4444/ffffff"
+}; //Creado para q funcione la vista perfil, cuando se haga la logica del json borrar
+
+const usersControllers = {
+
     	perfil: (req, res) => {	
-                res.render('users/perfil',);
+                res.render('users/perfil', {usuario: usuario});
         },
 
         login: (req, res) => {
@@ -11,6 +22,9 @@ const usersControllers =
         crear_cuenta: (req, res) => {
                 res.render('users/crear_cuenta');
         }
+
+		
 }
+
 
 module.exports = usersControllers;
