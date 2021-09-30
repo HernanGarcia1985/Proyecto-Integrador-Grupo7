958e3870a049f83cad0043c65431668470f20e29
Fabi88code <85083301+Fabi88code@users.noreply.github.com>
2021-09-29 21:25:48 -0300

    actualizacion para validar crear usuario

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,11 +1,15 @@
-const usersControllers = require('../controllers/usersControllers')
-
+const usersControllers = require('../controllers/usersControllers');
+const {body}= require ('express-validator');
+const validateCreate= [
+    body ('name', 'lastName').notEmpty().withMessage("Campo obligatorio"),
+    body ('email').isEmail().withMessage("Ingresé un mail válido"),
+];
 const express = require('express');
 const router = express.Router();
 
 router.get('/crear_cuenta',usersControllers.crear_cuenta);
 
-router.post('/crear_cuenta',usersControllers.procesoCrearCuenta);
+router.post('/crear_cuenta',validateCreate,usersControllers.procesoCrearCuenta);
 
 router.get('/login',usersControllers.login);
 
