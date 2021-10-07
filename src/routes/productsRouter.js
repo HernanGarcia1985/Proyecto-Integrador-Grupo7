bfc78db6d8429fe303b4b26a8d7d6a62306e0ea8
Fabi88code <85083301+Fabi88code@users.noreply.github.com>
2021-10-06 21:46:15 -0300

    rutas de productos con validaciones

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -38,7 +38,7 @@ router.get('/create', productsController.crear_producto); //products/create
 router.post('/create', validateCreate, productsController.store); 
 
 router.get('/edit/:id', productsController.editar_producto); //products/:id/edit cambio por comentario de jero al final de la clase
-router.put('/edit/:id', productsController.update); //products/:id cambio por comentario de jero al final de la clase
+router.put('/edit/:id', validateCreate, productsController.update); //products/:id cambio por comentario de jero al final de la clase
 
 router.delete('/edit/:id',productsController.destroy); //products/:id cambio por comentario de jero al final de la clase
 //router.delete('/:id', productsController.destroy); ver la ruta, así está en el proyecto de Jero 
