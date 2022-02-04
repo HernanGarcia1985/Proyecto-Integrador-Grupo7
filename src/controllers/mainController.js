92907e2ad5749355f6e176aa1689e4c49d03239e
HernanGarcia1985 <hernang1985@gmail.com>
2022-02-04 16:32:34 -0300

    Se configuro access_token con una variable de enterno y se seteo la preferencia para un producto generico

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,6 +3,12 @@ const path = require('path');
 //const {validationResult} = require('express-validator');
 const db = require('../database/models');
 const Sequelize = require('sequelize');
+const mercadopago = require ('mercadopago');
+
+
+mercadopago.configure({
+    access_token: process.env.PROD_ACCESS_TOKEN
+  });
 
 const mainControllers = 
 {
@@ -24,7 +30,39 @@ const mainControllers =
 
         carrito: (req, res) => {	
             res.render('carrito',);
-    }
+        },
+
+        mercadopago: (req,res) => {
+              
+              let preference = {
+                items: [
+                  {
+                    title: 'Mi producto',
+                    unit_price: 100,
+                    quantity: 1,
+                  }/*,
+                  {
+                    title: req.body.title,
+                    unit_price: parseInt(req.body.price),
+                    quantity: 1,
+                  }*/
+                ],
+                back_urls: {
+                    success: "http://localhost:3001/",
+                    failure: "http://localhost:3001/failure",
+                    pending: "http://localhost:3001/pending"
+                  }
+              };
+              
+              mercadopago.preferences.create(preference)
+              .then(function(response){
+              // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+              //global.id = response.body.id;
+                res.redirect(response.body.init_point);
+              }).catch(function(error){
+                console.log(error);
+              });
+        }
 
 }
 
